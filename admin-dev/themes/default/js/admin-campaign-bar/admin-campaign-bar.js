e4a1daa8d2b0d99e0f8e0f9142774317d309e0c7
Theo du Plooy <tdpcorp@gmail.com>
2023-11-13 22:19:57 +0200

    detect notification count

--- admin-dev/themes/default/js/admin-campaign-bar/admin-campaign-bar.js
+++ admin-dev/themes/default/js/admin-campaign-bar/admin-campaign-bar.js
@@ -1,18 +1,21 @@
 /**** Admin Campaign Bar JS ****/
 
+
 /// Check for notifications and show on responsive bell icon ///
 function checkForNotifications() {
-    console.log("checkForNotifications");
-    var checkForNotifications = $('.tb-admin-campaign-bar #header_notifs_icon_wrapper .notifs_badge span').html();
-    console.log("checkForNotifications NEW : " + checkForNotifications);
-    if ($(checkForNotifications) != 0) {
-        console.log("checkForNotifications: yes");
-        $(".tb-admin-campaign-bar-fa-icon .notifs_badge").addClass("fadeIn");
-    }
-    else {
-        $(".tb-admin-campaign-bar-fa-icon .notifs_badge").removeClass("fadeIn");
-        console.log("checkForNotifications: no");
-    }
+    $('.tb-admin-campaign-bar #header_notifs_icon_wrapper .notifs_badge span').each(function () {
+        var notificationCount = $(this).text();
+
+        if (parseInt(notificationCount) !== 0) {
+            $(this).closest('.notifs_badge').addClass('fadeIn');
+            console.log("Notification Count: " + notificationCount);
+
+        } else {
+            $(this).closest('.notifs_badge').removeClass('fadeIn');
+            console.log("Notification Count: " + notificationCount);
+
+        }
+    });
 }
 
 /// Notification code grab and inject into modal + open
